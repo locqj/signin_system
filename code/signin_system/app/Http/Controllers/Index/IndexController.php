27d54d9e42626305b76f3d2c286f2717421affa4
locqj <locqj@163.com>
2017-08-14 09:34:15 +0800

    2017-08-14 09:34:15 test wx_getuser_info

--- code/signin_system/app/Http/Controllers/Index/IndexController.php
+++ code/signin_system/app/Http/Controllers/Index/IndexController.php
@@ -18,6 +18,7 @@ class IndexController extends Controller
     {	
     	$dist = $this->preIndex();
     	$openid = session('user_id');
+    	return $openid;
     	if ($dist) {
 	        return view('index.index', compact('openid'));
     	}
